33048458544d03b1cd7adbd7c65b65bd6e76d940
taruncool <v.tarun.cool@gmail.com>
2014-10-30 11:17:55 +0530

    Update ebooks.php

--- ebooks.php
+++ ebooks.php
@@ -1,14 +1,9 @@
 <?php
-
 require_once('loader.php');
-$did=1;
-if($did)
-{
-$id=$did;
 
 $response["desis"]=array();
 
-$query=mysql_query("select * from ebooks where d_id='$id'");
+$query=mysql_query("select * from ebooks");
 
 if(mysql_num_rows($query)>0)
 {
@@ -16,22 +11,28 @@ while($row=mysql_fetch_array($query))
 {
 $id=$row['id'];
 $title=$row['title'];
-$image=$row['url'];
+$image=$row['img_path'];
+$url=$row['url'];
+
 if($image!='')
 {
-$image="http://192.168.0.104/live/uploads/".$row['url'];
+$image="http://avishkr.com/live/images/".$image;
 }
 else
 {
-$image="http://192.168.0.104/live/uploads/1.png";
+$image="http://avishkr.com/live/images/1.png";
+}
+if($url!='')
+{
+$url="http://avishkr.com/live/uploads/".$url;
 }
 
 $desis["id"]=$id;
 $desis["title"]=$title;
 $desis["image"]=$image;
+$desis["url"]=$url;
 
 array_push($response["desis"],$desis);
-
 }
 
 $response["success"]=1;
@@ -45,6 +46,5 @@ $response["message"]="No Users Found";
 
 }
 
-}
 
 ?>
